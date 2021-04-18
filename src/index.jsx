0e157877d9d964a5afb7f6820830e70f9db72553
lizuncong <2900170896@qq.com>
2021-04-18 20:58:11 +0800

    stash

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component, useState } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import Header from './Header';
 import Main from './Main';
 import { Provider } from './context';
 import styles from './index.module.less';
 
+const HeaderHook = () => {
+  const [num, addNum] = useState(0);
+  return (
+      <div>
+        这是一个hook组件：{num}
+        <div>
+          <button
+            onClick={() => {
+              console.log('hook.before..', num);
+              addNum(n =>  n + 1);
+              addNum(n =>  n + 1);
+              addNum(n =>  n + 1);
+              console.log('hook.after..', num);
+            }}
+          >
+            点击触发hook
+          </button>
+        </div>
+      </div>
+  )
+}
 export default class App extends Component{
   constructor(props){
     super(props);
@@ -15,29 +36,36 @@ export default class App extends Component{
 
   render(){
     const { count } = this.state;
-    const contextValue = {
-      count,
-      add: () => {
-        this.setState({ count: count + 1})
-      }
-    }
-    console.log('root..render')
+    const len = 3000;
     return (
-        <Provider value={contextValue}>
-          <div
-              onClick={() => {
-                this.setState({
-                  count: count + 1,
-                })
-              }}
-          >
-            root click me
+        <>
+          <div>
+            <HeaderHook />
+            <div>计数器：{ count }</div>
+            <button
+                onClick={() => {
+                  // console.log('before..', this.state.count);
+                  // this.setState({ count: count + 1});
+                  // console.log('after...',this.state.count)
+                  Promise.resolve(1).then(res => {
+                    console.log('before..', this.state.count);
+                    this.setState({ count: count + 1});
+                    console.log('after...',this.state.count)
+                  })
+                }}
+            >
+              add
+            </button>
           </div>
-          <Header />
-          <Main count={count} />
-        </Provider>
-    )
+          <ul>
+            {Array(len).fill(0).map((_, i) => <li>{i}</li>)}
+          </ul>
+        </>
+    );
   }
 }
 
-render(<App />, document.getElementById('root'))
+const root = document.getElementById('root');
+// render(<App />, root)
+ReactDOM.unstable_createRoot(root).render(<App/>);
+
