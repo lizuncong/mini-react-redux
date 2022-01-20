08c782eeddab05b64f4e136a0bc40ec2ebb09857
lizuncong <2900170896@qq.com>
2022-01-20 23:33:54 +0800

    feat: applyMiddleware stash

--- redux/applyMiddleware.js
+++ redux/applyMiddleware.js
@@ -1,3 +1,67 @@
+// createStore(reducer, preloadedState, enhancer)
+// => createStore(reducer, preloadedState, applyMiddleware(...middlewares))
+// => enhancer(createStore)(reducer, preloadedState)
+// 插件执行有点类似koa的洋葱圈模型，从左往右执行，直到真正的store.dispatch
+const applyMiddleware = (...middlewares) => {
+  return (createStore) => {
+    return (reducer, preloadedState) => {
+      const store = createStore(reducer, preloadedState)
+      const { getState } = store;
+      const mids = middlewares.map(mid => mid({ getState }))
+      console.log('mids===', mids)
+      // const dispatch = store.dispatch
+      const next = (action) => {
+        if(i === middlewares.length){
+          return store.dispatch;
+        }
+        const mid = middlewares[i]
+        return mid(next)
+      }
+      let dispatch = (action) => {
+        console.log('test=====', action)
+        next(0)
+      }
+      return {
+        ...store,
+        dispatch
+      }
+    }
+  }
+}
+
+export default applyMiddleware
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Composes single-argument functions from right to left. The rightmost
  * function can take multiple arguments as it provides the signature for
@@ -8,42 +72,42 @@
  * from right to left. For example, compose(f, g, h) is identical to doing
  * (...args) => f(g(h(...args))).
  */
-
-export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return (arg) => arg
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
-
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
-}
-
-function applyMiddleware(...middlewares) {
-  return (createStore) => (reducer, preloadedState) => {
-    const store = createStore(reducer, preloadedState)
-    let dispatch = () => {
-      throw new Error(
-          'Dispatching while constructing your middleware is not allowed. ' +
-          'Other middleware would not be applied to this dispatch.'
-      )
-    }
-
-    const middlewareAPI = {
-      getState: store.getState,
-      dispatch: (...args) => dispatch(...args),
-    }
-    const chain = middlewares.map((middleware) => middleware(middlewareAPI))
-    dispatch = compose(...chain)(store.dispatch)
-
-    return {
-      ...store,
-      dispatch,
-    }
-  }
-}
-
-
-export default applyMiddleware
+//
+// export default function compose(...funcs) {
+//   if (funcs.length === 0) {
+//     return (arg) => arg
+//   }
+//
+//   if (funcs.length === 1) {
+//     return funcs[0]
+//   }
+//
+//   return funcs.reduce((a, b) => (...args) => a(b(...args)))
+// }
+//
+// function applyMiddleware(...middlewares) {
+//   return (createStore) => (reducer, preloadedState) => {
+//     const store = createStore(reducer, preloadedState)
+//     let dispatch = () => {
+//       throw new Error(
+//           'Dispatching while constructing your middleware is not allowed. ' +
+//           'Other middleware would not be applied to this dispatch.'
+//       )
+//     }
+//
+//     const middlewareAPI = {
+//       getState: store.getState,
+//       dispatch: (...args) => dispatch(...args),
+//     }
+//     const chain = middlewares.map((middleware) => middleware(middlewareAPI))
+//     dispatch = compose(...chain)(store.dispatch)
+//
+//     return {
+//       ...store,
+//       dispatch,
+//     }
+//   }
+// }
+//
+//
+// export default applyMiddleware
--- test/applyMiddleware.js
+++ test/applyMiddleware.js
@@ -1,5 +1,6 @@
+import { applyMiddleware } from 'redux'
 import createStore from '../redux/createStore'
-import applyMiddleware from '../redux/applyMiddleware'
+// import applyMiddleware from '../redux/applyMiddleware'
 function counter(state = { value: 0 }, action) {
   switch (action.type) {
     case 'counter/incremented':
@@ -12,10 +13,10 @@ function counter(state = { value: 0 }, action) {
 }
 
 
-function logger({ getState }) {
+function logger({ getState, dispatch }) {
   return next => action => {
-    console.log('will dispatch', action)
-
+    console.log('will dispatch', action, next)
+    // dispatch(action)
     // Call the next dispatch method in the middleware chain.
     const returnValue = next(action)
 
@@ -27,8 +28,24 @@ function logger({ getState }) {
   }
 }
 
+function logger2({ getState }) {
+  return next => action => {
+    console.log('will dispatch2', action, next)
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action)
+
+    console.log('state after dispatch2', getState())
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue
+  }
+}
+
 // createStore返回值{ subscribe, dispatch, getState }.
-let store = createStore(counter, undefined, applyMiddleware(logger))
+// 第三个参数 enhancer(createStore)(reducer, preloadedState)
+let store = createStore(counter, undefined, applyMiddleware(logger, logger2))
 
 store.subscribe(() => console.log('test===1',store.getState()))
 
